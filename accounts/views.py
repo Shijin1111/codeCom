52832b23e0e22f659d2b2b80d18005a5a7ef7aa9
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2024-10-22 06:02:48 +0530

    login done

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+# Custom login view to handle login form rendering
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('home'))  # Redirect to a protected page after login
+            else:
+                form.add_error(None, "Invalid credentials")
+        else:
+            form.add_error(None, "Invalid credentials")
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
+
+# Custom logout view
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))  # Redirect to login page after logout
+
+def homepage_view(request):
+    return render(request,'base.html')
--- codeCom/settings.py
+++ codeCom/settings.py
@@ -14,8 +14,10 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = BASE_DIR/"accounts/templates"
 
-
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'codeCom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = BASE_DIR/"accounts/static" 
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
